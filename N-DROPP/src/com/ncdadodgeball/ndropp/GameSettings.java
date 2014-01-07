8979423f01db3a4c311cad0f0a76e06db9c33df8
kpninja12 <peltikyl12@gmail.com>
2014-01-06 19:38:19 -0500

    project properties modifications. adding concept statement & ux design doc.

---
+++ N-DROPP/src/com/ncdadodgeball/ndropp/GameSettings.java
@@ -0,0 +1,116 @@
+package com.ncdadodgeball.ndropp;
+
+public class GameSettings {
+
+	boolean m_bVolume;
+	boolean m_bShotClkCountUp;
+	boolean m_bShotClkAudio;
+	boolean m_bVibration;
+	boolean m_bHomeTeamLeft;
+	int m_iTimeOuts;
+	long m_lShotClock;	//shot clock in milliseconds
+	long m_lGameClock;	//game clock in milliseconds
+
+	public GameSettings(){
+		m_bVolume = true;
+		m_bShotClkCountUp = true;
+		m_bShotClkAudio = false;
+		m_bVibration = false;
+		m_bHomeTeamLeft = true;
+		m_iTimeOuts = 2;
+		m_lShotClock = 15000;		//shot clock 15 seconds
+		m_lGameClock = 1500000;		//game clock at 25 minutes
+	}
+	
+	
+	public boolean isM_bVolume() {
+		return m_bVolume;
+	}
+
+
+
+	public void setM_bVolume(boolean m_bVolume) {
+		this.m_bVolume = m_bVolume;
+	}
+
+
+
+	public boolean isM_bShotClkCountUp() {
+		return m_bShotClkCountUp;
+	}
+
+
+
+	public void setM_bShotClkCountUp(boolean m_bShotClkCountUp) {
+		this.m_bShotClkCountUp = m_bShotClkCountUp;
+	}
+
+
+
+	public boolean isM_bShotClkAudio() {
+		return m_bShotClkAudio;
+	}
+
+
+
+	public void setM_bShotClkAudio(boolean m_bShotClkAudio) {
+		this.m_bShotClkAudio = m_bShotClkAudio;
+	}
+
+
+
+	public boolean isM_bVibration() {
+		return m_bVibration;
+	}
+
+
+
+	public void setM_bVibration(boolean m_bVibration) {
+		this.m_bVibration = m_bVibration;
+	}
+
+
+
+	public boolean isM_bHomeTeamLeft() {
+		return m_bHomeTeamLeft;
+	}
+
+
+
+	public void setM_bHomeTeamLeft(boolean homeTeamLeft) {
+		m_bHomeTeamLeft = homeTeamLeft;
+	}
+
+
+
+	public int getM_iTimeOuts() {
+		return m_iTimeOuts;
+	}
+
+
+
+	public void setM_iTimeOuts(int timeOuts) {
+		m_iTimeOuts = timeOuts;
+	}
+	
+	
+	public long getShotClock() {
+		return m_lShotClock;
+	}
+
+
+	public void setShotClock(long milliseconds) {
+		m_lShotClock = milliseconds;
+	}
+
+
+	public long getGameClock() {
+		return m_lGameClock;
+	}
+
+
+	public void setGameClock(long milliseconds) {
+		m_lGameClock = milliseconds;
+	}
+
+}
---
+++ N-DROPP/src/com/ncdadodgeball/ndropp/RulebookActivity.java
@@ -0,0 +1,82 @@
+package com.ncdadodgeball.ndropp;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.ncdadodgeball.ndropp.R;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class RulebookActivity extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		File rootFilesDir = getFilesDir();
+		Log.e("NCDA App directory internal directory: " + rootFilesDir.getAbsolutePath(), "");
+		
+		File rulebook = new File(rootFilesDir.getAbsolutePath() + "/rulebook.pdf");
+		
+		//DOWNLOAD RULEBOOK PDF -- WORKS!! :)
+		URL url = null;
+		HttpURLConnection socket= null;
+		try {
+			url = new URL("http://www.ncdadodgeball.com/rulebook/ncda-rules.pdf");
+			socket = (HttpURLConnection) url.openConnection();
+			socket.setInstanceFollowRedirects(true);
+			socket.setDoInput(true);
+			socket.connect();
+			InputStream istream = new BufferedInputStream(socket.getInputStream());
+			OutputStream ostream = new FileOutputStream(rulebook);
+			byte [] input = new byte[1024];
+			while(istream.read(input, 0, 1024) != -1){
+				ostream.write(input);
+			}
+		}
+		catch (Exception e) {
+			Log.d("NCDA App", e.getMessage());
+			e.printStackTrace();
+		}
+		finally{
+			socket.disconnect();
+		}
+		
+		
+		//COPY DOWNLOADED PDF FROM PRIVATE MEMORY TO PUBLIC MEMORY --> NEEDED FOR TESTING. CONFIRMED --> IT WORKS !! :)
+		File publicRulebook = new File((Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)).getAbsolutePath() + "/rulebook.pdf");
+		
+		try{
+			InputStream istream = new FileInputStream(rulebook.getAbsolutePath());
+			OutputStream ostream = new FileOutputStream(publicRulebook);
+			byte [] input = new byte[1024];
+			
+			while(istream.read(input, 0, 1024) != -1){
+				ostream.write(input);
+			}
+			ostream.close();
+			istream.close();
+		}
+		catch(Exception e){
+			Log.d("NCDA app", e.getMessage());
+		}
+		
+		setContentView(R.layout.rulebook);		//still no rulebook yet. to come, to come.
+
+	}
+}
